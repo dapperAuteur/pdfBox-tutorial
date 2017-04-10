9bf7694e6e1f9fac3ba519a64077166768ecfbdd
awe <aweful@awews.com>
2017-04-09 21:19:29 -0700

    create pdf document with multiple pages

--- src/main/java/com/aweful/PdfBox/App.java
+++ src/main/java/com/aweful/PdfBox/App.java
@@ -3,6 +3,7 @@ package com.aweful.PdfBox;
 import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 
 /**
  * Hello world!
@@ -14,6 +15,10 @@ public class App
     {
         System.out.println( "Hello World!" );
         PDDocument document = new PDDocument();
+        for(int i=0; i<10; i++){
+        	PDPage my_page = new PDPage();
+        	document.addPage(my_page);
+        }
         document.save("C:\\Users\\amcdonald\\Desktop\\myDoc.pdf");
         System.out.println("PDF Created");
         document.close();
